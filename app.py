4f59b36e76d4845d571e3a3653009c561b877300
shaheer <alishaheer272002@gmail.com>
2026-07-08 16:02:03 +0530

    updated application message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app= Flask(__name__)
 @app.route('/')
 
 def home():
-    return "Task 22 gitops demo application version testing"
+    return "Task 22 gitops demo application complete version testing"
 
 
 if __name__ == "__main__":
